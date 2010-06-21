c18fd0eaf0fc95ed86aba9a86f5507580401ea66
ChrisSalij <Chris@Salij.org>
2010-06-21 16:07:06 +0100

    Changed the way that pages were fetched. It used to rely on every query returning 500 revisions, but for hight volume/large pages, it can return an irregular amount. This takes that into account and now gets as close to the number of revisions that are requested, plus a small margin of error (max +500) This change also deals properly with the case where there are fewer revisions than the target. It gets all the revisions and then gracefully dies.

--- scrape.rb
+++ scrape.rb
@@ -1,7 +1,7 @@
 # This code is not pretty. It is not optimised, but it works.
 ["rubygems", "curb", "bot_login", "wiki_login"].each {|x| require x}
 
-revisions = 20
+revisions_to_get = 10000
 pages = File.read("pages.txt").split
 
 # Truncates the head of an xml article making it
@@ -37,54 +37,49 @@ end
 # Go to wikipedia and download the data for the various pages that were requested
 puts "Fetching data from Wikipedia for the following pages"
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------# 
-revs_per_query = 500                    # Set the maximum number of revisions per query to get (Max 500)
-queries = revisions/revs_per_query      # Determine the number of queries to make
-remainder = revisions%revs_per_query    # See if the number of revisions to get divides equally into the number of queries
-if remainder > 0                        # If not then make an extra query to cover the remainder
-  queries += 1
-end 
-
-download_time = {}                      # Tracks the time taken for each individual download. Not used at the moment
+revisions_per_query = 500                     # Set the maximum number of revisions per query to get (Max 500)
+download_time = {}                            # Tracks the time taken for each individual download. Not used at the moment
 total_timer_start = Time.now
+overall_revision_count = 0
 
 pages.each do |page|
   puts "  ✓ #{page.gsub("_", " ")}"
   page_timer_start = Time.now
   @page_data = ""
-  cont_rev =0
+  last_rev = 0
+  revision_count = 0
   
-  queries.times do |time|
-    if cont_rev != -1                         # We reached the end of the history, no point in querying again
-      if remainder != 0 && time == queries-1  # If there is a remainder and we're on the last query
-        revs_per_query = remainder+1          # Only get the amount of revisions in the remainder
-      end
-      if cont_rev == 0                        # If we're on the first query, get the last X revisions starting at the most recent revision available
-        query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=#{page}&rvprop=ids|timestamp|user|comment|content&rvlimit=#{revs_per_query}&format=xml"
-      else                                    # If we're on a subsequent query, get the last X revisions starting at the revision previous to the oldest one we have on record
-        query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=#{page}&rvprop=ids|timestamp|user|comment|content&rvlimit=#{revs_per_query-1}&rvstartid=#{cont_rev-1}&format=xml"
-        @page_data = remove_tail @page_data
-      end
-      
-      text = Curl::Easy.new query_url do |curl|
-        curl.cookies = @cookies
-        curl.headers = {"User-Agent" => @user_agent}
-        curl.perform
-      end # puts text.body_str
-      
-      if Rev.parse(text.body_str).last.nil?   # Then we've gone back as far as we can.
-        @page_data += @tail                   # Add the tail back on that was chopped off
-        cont_rev = -1                         # Make sure that no more calls are made on this page
-      else                                    # Don't both parsing it as there is nothing to parse
-        if cont_rev != 0                      # If this is not the first query
-          @page_data +=  remove_head text.body_str
-        else
-          @page_data += text.body_str
-        end
-        File.open("pages/#{page}.xml", "w"){|f| f.write(text.body_str)} 
-        cont_rev = Rev.parse(text.body_str).last.revid # puts cont_rev
-      end
+  begin
+    if revisions_to_get < revisions_per_query
+      revisions_to_get = revisions_per_query
     end
-  end
+    if last_rev == 0                          # If we're on the first query, get the last X revisions starting at the most recent revision available
+      query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=#{page}&rvprop=ids|timestamp|user|comment|content&rvlimit=#{revisions_per_query}&format=xml"
+    else                                      # If we're on a subsequent query, get the last X revisions starting at the revision previous to the oldest one we have on record
+      query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=#{page}&rvprop=ids|timestamp|user|comment|content&rvlimit=#{revisions_per_query}&rvstartid=#{last_rev-1}&format=xml"
+      @page_data = remove_tail @page_data
+    end
+    
+    text = Curl::Easy.perform query_url do |curl|       # Make the request
+      curl.cookies = @cookies                       # Set the login Cookies
+      curl.headers = {"User-Agent" => @user_agent}
+    end # puts text.body_str
+    
+    revisions_this_query = Rev.parse(text.body_str).length  # The amount of revisions returned in this query
+    revision_count += revisions_this_query
+    
+    if revisions_this_query == 0              # Then we've gone back as far as we can.
+      @page_data += @tail                     # Add the tail back on that was chopped off
+    else                                      # Don't both parsing it as there is nothing to parse
+      if last_rev != 0                        # If this is not the first query
+        @page_data +=  remove_head text.body_str
+      else
+        @page_data += text.body_str
+      end
+      last_rev = Rev.parse(text.body_str).last.revid
+    end
+    puts "    # of Revisions: #{revisions_this_query}"
+  end while revision_count < revisions_to_get && revisions_this_query != 0
   
   # This does a search and replace on the retrieved text. It makes it much simpler to traverse later on.
   @page_data.gsub! 'xml:space="preserve">', 'xml:space="preserve"><text>'
@@ -92,8 +87,14 @@ pages.each do |page|
   
   File.open("pages/#{page}.xml", "w"){|f| f.write(@page_data)} 
   download_time[page] = Time.now - page_timer_start
-  puts "# of Revisions: #{Rev.parse(File.read("pages/#{page}.xml")).length}"
+  total_rev_count = Rev.parse(File.read("pages/#{page}.xml")).length
+  
+  if total_rev_count == revision_count
+    puts "    - Total # of Revisions: #{Rev.parse(File.read("pages/#{page}.xml")).length}"
+  else
+    puts "    Shit something went wrong. Our count is off"
+  end
+  overall_revision_count += total_rev_count
 end
-puts "Time to complete this run: #{Time.now - total_timer_start}"
-
-#puts "\nDone Downloading Data. Downloaded a total of #{revisions*pages.length} revisions over #{pages.length} pages\n"
+puts "A total of #{overall_revision_count}s were downloaded across #{pages.size} pages"
+puts "Time to complete this run: #{(Time.now-total_timer_start)/60} minutes"
