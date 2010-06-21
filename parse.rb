8ca6e1883cb658c250c6053218e380a5ee865d55
ChrisSalij <Chris@Salij.org>
2010-06-21 16:03:44 +0100

    A bit of cleanup

--- parse.rb
+++ parse.rb
@@ -1,5 +1,5 @@
 # This parses the information in an xml doc downloaded using the scrape2.rb script
-# This is the script that will extract all the information we want
+# This is the script that will extract all the information we want.
 ["rubygems", "happymapper", "digest/sha1", "functions", "xml_definitions"].each {|x| require x}
 pages = File.read("pages.txt").split
 
@@ -8,46 +8,31 @@ pages.each do |page|
   
   begin
     xml_string = File.read "pages/#{page}.xml"
-  rescue => err #Oh shit, something went wrong. Deal with it.
+  rescue => err # Oh shit, something went wrong. Deal with it.
     puts "Oh fiddle-sticks, something went wrong! Try running the scrape.rb file again to make sure that we have all the files we're looking for"
     err
   end
   
   puts "\n------------------------------------------------- \n  #{page.capitalize}:\n "
   revs = []
-  revisions = Rev.parse xml_string # Use HappyMapper to make an array of the revisions
+  revisions = Rev.parse xml_string  # Use HappyMapper to make an array of the revisions
   
   revisions.reverse.each do |rev|
-    # What must be done on each revision
-    @user_hash[rev.user] = @user_hash.fetch(rev.user, 0)+1    # Collect user information
-    rev.hash = Digest::SHA1.hexdigest rev.text                # Set the hash value (a hash of the text) for each revisions
+    process_revision rev, revs      # What must be done on each revision
     
-    revert? rev, revs
-    if revs.length > 2
-      compute_intermediate_revision rev, revs.last, revs
-      # process_text @first, @second, @third
-    end
-    revs.push rev
-    
-    
-    # puts rev.comment
-    # puts "Text:       #{rev.text}"
-    # puts "Revid:      #{rev.revid.to_s}, Parentid:  #{rev.parentid.to_s}"
-    # puts "User:       #{rev.user} "
-    # puts "Timestamp:  #{rev.timestamp.to_s}"
-    # puts "Comment:    #{rev.comment}"
-    # puts "-----------------------------------------------"
     puts "------"
   end
 end
-# puts Time.parse(rev.timestamp).to_i
-# process_user_hash user_hash
-# 
+
+# process_user_hash
+# puts @reg_hash.count
+# puts @unreg_hash.count
+
 # puts "List of all users"
-# user_hash.keys.sort.each { |key| puts "  #{key}:     "+user_hash[key].to_s }
+# @user_hash.keys.sort.each { |key| puts "  #{key}:     " +@user_hash[key].to_s }
 # 
 # puts "List of registered users"
-# @reg_hash.keys.sort.each { |key| puts "  #{key}:     "+user_hash[key].to_s }
+# @reg_hash.keys.sort.each { |key| puts "  #{key}:     "  +@user_hash[key].to_s }
 # 
 # puts "List of unregistered IPs"
-# @unreg_hash.keys.sort.each { |key| puts "  #{key}:     "+user_hash[key].to_s }
+# @unreg_hash.keys.sort.each { |key| puts "  #{key}:     "+@user_hash[key].to_s }
