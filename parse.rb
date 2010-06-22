4a8e55b72822312be09ec7ef737686ff4578df66
ChrisSalij <Chris@Salij.org>
2010-06-22 12:09:50 +0100

    Now creates a file based on the revision tempalte

--- parse.rb
+++ parse.rb
@@ -5,7 +5,7 @@ pages = File.read("pages.txt").split
 
 pages.each do |page|
   @user_hash = {}; @unreg_hash = {}; @reg_hash = {} # Initialise/Empty User Hashes for each page
-  
+  @output = "<?xml version=\"1.0\"?>"
   begin
     xml_string = File.read "pages/#{page}.xml"
   rescue => err # Oh shit, something went wrong. Deal with it.
@@ -16,14 +16,48 @@ pages.each do |page|
   puts "\n------------------------------------------------- \n  #{page.capitalize}:\n "
   revs = []
   revisions = Rev.parse xml_string  # Use HappyMapper to make an array of the revisions
+  @output += "<name>#{page}</name>"
+  @output += "<revisions>"
   
   revisions.reverse.each do |rev|
-    process_revision rev, revs      # What must be done on each revision
+    revert = process_revision rev, revs
+    
+    if !revert.nil?
+      @output += "<rev revertid=\"#{revert}\">"
+    else
+      @output += "<rev revertid=\"\">"
+    end
+    
+    @output += "<revid>#{rev.revid}</revid>"
+    @output += "<parentid>#{rev.parentid}</parentid>"
+    @output += "<user>#{rev.user}</user>"
+    @output += "<timestamp>#{rev.timestamp}</timestamp>"
+    @output += "<unixtime>#{Time.parse(rev.timestamp).to_i}</unixtime>"
+    @output += "<age>#{rev.age}</age>"
+    @output += "<comment>#{rev.comment}</comment>"
+    @output += "<text>#{rev.text}</text>"
+    @output += "</rev>"
+    
+#   <revisions>                     <!-- A list of all of the revisions of this page -->
+#      <rev revertid="" >          <!-- An individual revision. Attribute stores whether this revision is a revert, and it which revision is it a revert to. -->
+#        <revid></revid>               <!-- The id of a revision -->
+#        <parentid></parentid>             <!-- The parent if of this revision -->
+#        <user></user>               <!-- The user that made this particular revision -->
+#        <timestamp></timestamp>           <!-- The date-time timestamp of this revision -->
+#        <unixtime></unixtime>           <!-- The unix timestamp of this revision -->
+#        <age></age>                 <!-- The edit age of the revision -1 for the most recent age-->
+#        <comment></comment>             <!-- The comment included with this revision -->
+#        <text xml:space="preserve"></text>      <!-- The text of this revision -->
+#      </rev>
+#    </revisions>
+
+    #process_revision rev, revs      # What must be done on each revision
     
     puts "------"
   end
+  @output += "</revisions></page>"
+  File.open("data/page_#{page}.xml", "w"){|f| f.write(@output)} 
 end
-
 # process_user_hash
 # puts @reg_hash.count
 # puts @unreg_hash.count
