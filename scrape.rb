ba354875cd40b6a49417b93045cd6907555e53a2
ChrisSalij <Chris@Salij.org>
2010-06-24 15:12:47 +0100

    Finally got the bastard working

--- scrape.rb
+++ scrape.rb
@@ -1,7 +1,7 @@
 # This code is not pretty. It is not optimised, but it works.
 ["rubygems", "curb", "bot_login", "wiki_login"].each {|x| require x}
 
-revisions_to_get = 7000
+revisions_to_get = 200
 pages = File.read("pages.txt").split
 
 # Truncates the head of an xml article making it
@@ -10,7 +10,7 @@ def remove_head str, state
   if state.eql? "rev"
     match_str = "<rev revid="
   elsif state.eql? "bl"
-    match_str = "<bl pageid"
+    match_str = "<bl pageid="
   end
   i = 0; cont = "go"
   begin
@@ -19,7 +19,7 @@ def remove_head str, state
       cont = "stop"
     end
     i += 1
-  end while cont != "stop"
+  end while cont != "stop" && i < str.length
   return str
 end
 
@@ -41,13 +41,14 @@ def remove_tail str, state
       cont = "stop"
     end
     i -= 1
-  end while cont != "stop"
+  end while cont != "stop" && i > 0
   return str
 end
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------# 
 # Go to wikipedia and download the data for the various pages that were requested
 puts "\nFetching data from Wikipedia for the following pages"
+puts "  Page:   Revs,   Links,  Done"
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------# 
 revisions_per_query = 500                     # Set the maximum number of revisions per query to get (Max 500)
 download_time = {}                            # Tracks the time taken for each individual download. Not used at the moment
@@ -56,15 +57,17 @@ overall_revision_count = 0
 overall_link_count = 0
 
 pages.each do |page|
-  puts "  ✓ #{page.gsub("_", " ")}"
+  STDOUT.sync = true
+  print " #{page.gsub("_", " ")}"
   page_timer_start = Time.now
-  @page_data = ""
   last_rev = 0
   last_link = 0
   revision_count = 0
   link_count = 0
   
+  File.open("pages/#{page}.xml", "w"){|f| f.write("")}
   begin   # Get the revisions
+    page_data = ""
     if revisions_to_get < revisions_per_query
       revisions_to_get = revisions_per_query
     end
@@ -72,7 +75,6 @@ pages.each do |page|
       query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=#{page}&rvprop=ids|timestamp|user|comment|content&rvlimit=#{revisions_per_query}&format=xml"
     else                                      # If we're on a subsequent query, get the last X revisions starting at the revision previous to the oldest one we have on record
       query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=#{page}&rvprop=ids|timestamp|user|comment|content&rvlimit=#{revisions_per_query}&rvstartid=#{last_rev-1}&format=xml"
-      @page_data = remove_tail @page_data, "rev"
     end
     
     text = Curl::Easy.perform query_url do |curl|       # Make the request
@@ -80,30 +82,32 @@ pages.each do |page|
       curl.headers = {"User-Agent" => @user_agent}
     end # puts text.body_str
     
+    page_data = text.body_str
     revisions_this_query = Rev.parse(text.body_str).length  # The amount of revisions returned in this query
     revision_count += revisions_this_query
     
-    if revisions_this_query == 0              # Then we've gone back as far as we can.
-      @page_data += @tail                     # Add the tail back on that was chopped off
-    else                                      # Don't both parsing it as there is nothing to parse
-      if last_rev != 0                        # If this is not the first query
-        @page_data +=  remove_head text.body_str, "rev"
-      else
-        @page_data += text.body_str
-      end
-      last_rev = Rev.parse(text.body_str).last.revid
+    if last_rev != 0
+      page_data = remove_head page_data, "rev"
     end
+    
+    if revisions_this_query != 0
+      page_data = remove_tail page_data, "rev"
+      
+      # This does a search and replace on the retrieved text. It makes it much simpler to traverse later on.
+      page_data.gsub! 'xml:space="preserve">', 'xml:space="preserve"><text>'
+      page_data.gsub! '</rev>', '</text></rev>'
+      # puts page_data[0..300]
+      File.open("pages/#{page}.xml", "a"){|f| f.write(page_data)} # Append the current set of revisions to the existing ones
+    end
+    
+    last_rev = Rev.parse(text.body_str).last.revid
     #puts "    # of Revisions: #{revisions_this_query}"
-  end while revision_count < revisions_to_get && revisions_this_query > 0
+  end while revision_count < revisions_to_get && revisions_this_query != 0
+  File.open("pages/#{page}.xml", "a"){|f| f.write("</revisions></page></pages></query><query-continue><revisions rvstartid=\"357322858\" /></query-continue></api>")}
   
-  # This does a search and replace on the retrieved text. It makes it much simpler to traverse later on.
-  @page_data.gsub! 'xml:space="preserve">', 'xml:space="preserve"><text>'
-  @page_data.gsub! '</rev>', '</text></rev>'
-  
-  File.open("pages/#{page}.xml", "w"){|f| f.write(@page_data)} 
   download_time[page] = Time.now - page_timer_start
   total_rev_count = Rev.parse(File.read("pages/#{page}.xml")).length
-  puts "      + #{total_rev_count} revisions Downloaded"
+  print ":  #{total_rev_count}"
   
   @page_data = ""
   begin  # Get the Link
@@ -137,7 +141,7 @@ pages.each do |page|
   File.open("pages/#{page}_links.xml", "w"){|f| f.write(@page_data)}
   download_time[page] = Time.now - page_timer_start
   total_link_count = Bl.parse(File.read("pages/#{page}_links.xml")).length
-  puts "      + #{total_link_count} links Downloaded"
+  print ",    #{total_link_count}"
   
   # if total_rev_count == revision_count
   #   puts "    - Total # of Revisions: #{Rev.parse(File.read("pages/#{page}.xml")).length}"
@@ -145,10 +149,11 @@ pages.each do |page|
   #   puts "    Shit something went wrong. Our count is off"
   # end
   # overall_revision_count += total_rev_count
-  # overall_link_count += total_link_count  
+  # overall_link_count += total_link_count
+  puts "  ✓" 
 end
 
-puts
+puts "\a"
 puts "A total of #{overall_revision_count} revisions were downloaded across #{pages.size} pages"
 puts "A total of #{overall_link_count} linkss were downloaded across #{pages.size} pages"
 puts "Time to complete this run: #{(Time.now-total_timer_start)/60} minutes"
