7c2a5fad33a079b1c1e9db6ab1ef762d2fa451da
ChrisSalij <Chris@Salij.org>
2010-06-24 15:13:41 +0100

    Identifies which users reverted text by which author. Still a work in progress

--- step3.rb
+++ step3.rb
@@ -1,13 +1,35 @@
 ["rubygems", "happymapper", "definitions/xml_definitions"].each {|x| require x}
-
+puts "------------------------------------------------------------------------------------------"
 pages = File.read("pages.txt").split
 
 pages.each do |page|
+  revert_count = 0
+  prev_revert_id = 0
   revisions = Revision.parse File.read"data/revisions_#{page}.xml"
-  revisions.each do |revision|
-    if !revision.revertid.nil?
-      puts revision.revertid
+  revisions.reverse.each do |revision|
+    if prev_revert_id === revision.revid
+      puts "-- #{revision.user} #{revision.revid} I am the king"
+      prev_revert_id = 0
+    end 
+
+    if prev_revert_id != 0 && revision.revertid.nil?
+      puts "  #{revision.user} just got his edit reverted #{revision.revid}"
     end
+    
+    if !revision.revertid.nil?
+      revert_count +=1
+      if prev_revert_id == revision.revertid
+        puts "#{revision.user}'s revision #{revision.revid} is a revert back to #{revision.revertid}"
+      else
+        puts 
+        puts "#{revision.user}'s revision #{revision.revid} is a revert back to #{revision.revertid}"
+      end
+      prev_revert_id = revision.revertid
+    else
+      #puts "intermediate revision #{revision.revid} - #{revision.user}"
+    end
+    
   end
+  puts "\n#{page} has #{revert_count} reverts over #{revisions.length} revisions"
   
 end
