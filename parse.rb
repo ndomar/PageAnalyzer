04722ef25277260e172390d987be7fcbb68f1ac9
ChrisSalij <Chris@Salij.org>
2010-06-29 16:28:40 +0100

    Some minor bug fixes

--- parse.rb
+++ parse.rb
@@ -1,18 +1,26 @@
 # This parses the information in an xml doc downloaded using the scrape2.rb script
 # This is the script that will extract all the information we want.
-["rubygems", "happymapper", "digest/sha1", "functions", "definitions/xml_definitions"].each {|x| require x}
+["rubygems", "happymapper", "digest/sha1", "functions", "definitions/xml_definitions", "amatch"].each {|x| require x}
+include Amatch
+
+@folder = "parsed_data"
 pages = File.read("pages.txt").split
 start = Time.now
 
+if !File.directory? @folder # If a directectory called pages does not exist in the current folder, create it.
+  puts "Created the 'pages' folder to store downloaded data"
+  Dir.mkdir @folder
+end
+
 pages.each do |page|
   @user_hash = {}; @unreg_hash = {}; @reg_hash = {} # Initialise/Empty User Hashes for each page
-  File.open("data/revisions_#{page}.xml", "w"){|f| f.write("<?xml version=\"1.0\"?><revisions>")}
+  File.open("#{@folder}/revisions_#{page}.xml", "w"){|f| f.write("<?xml version=\"1.0\"?><revisions>")}
   @user_file = "<?xml version=\"1.0\"?><user>"
-  File.open("data/page_#{page}.xml", "w"){|f| f.write("<?xml version=\"1.0\"?><page><name>#{page}</name><revisions></revisions></page>")}
+  File.open("#{@folder}/page_#{page}.xml", "w"){|f| f.write("<?xml version=\"1.0\"?><page><name>#{page}</name><revisions></revisions></page>")}
   
   begin
-    data_string = File.read "pages/#{page}.xml"
-    link_string = File.read "pages/#{page}_links.xml"
+    data_string = File.read "scraped_data/#{page}.xml"
+    link_string = File.read "scraped_data/#{page}_links.xml"
   rescue => err # Oh shit, something went wrong. Deal with it.
     puts "Oh fiddle-sticks, something went wrong! Try running the scrape.rb file again to make sure that we have all the files we're looking for"
     err
@@ -23,15 +31,21 @@ pages.each do |page|
   revisions = Rev.parse data_string              # Use HappyMapper to make an array of the revisions
   
   revisions.reverse.each do |rev|
-    if !rev.text.nil?
-      process_revision rev, revs, page         # What must be done on each revision@revision_file
+    
+    if !rev.text.nil?      
+      process_revision rev, revs, page         # What must be done on each revision/revision_file
       user_add_revision rev.user, page, rev.revid
       page_add_revision page, rev.user, rev.revid
       # puts "------"
     end
+    # if !@prev.nil?
+    #   puts @prev.text.levenshtein_similar rev.text
+    # end
+    # @prev = rev
   end
+  puts "Finised Parsing Revisions"
   
-  File.open("data/revisions_#{page}.xml", "a"){|f| f.write("</revisions>")}
+  File.open("#{@folder}/revisions_#{page}.xml", "a"){|f| f.write("</revisions>")}
   
   links = Bl.parse link_string
   link_text = "<links>"
@@ -40,6 +54,7 @@ pages.each do |page|
   end
   link_text += "</links>"
   page_add_links page, link_text
+  puts "Finised Parsing Links"
   
 end
 puts Time.now-start
