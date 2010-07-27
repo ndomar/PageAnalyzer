a0c7ad64454676f62cb8cd1dd577dd7c1e54e2b4
ChrisSalij <Chris@Salij.org>
2010-07-27 13:04:15 +0100

    First work on a script to create a csv file for a page which will be used for Weka analysis

---
+++ create_page_csv.rb
@@ -0,0 +1,28 @@
+require "functions"
+
+if ARGV[0] then user_file = ARGV[0] else user_file = "user.csv" end
+File.open(user_file, "w"){|f| f.write("Pagename,User Count,Reliable User Count,Reliable User Percentage,Reliable User Edit Count,Reliable User Edit Percentage,Other User Count,Other User Percentage,Other User Edit Count,Other User Edit Percentage,Revert Count,Link Count\n")}
+
+
+start = Time.now
+  Pagename = 0
+  User Count = 0
+  Reliable User Count = 0
+  Reliable User Percentage = 0
+  Reliable User Edit Count = 0
+  Reliable User Edit Percentage = 0
+  Other User Count = 0
+  Other User Percentage = 0
+  Other User Edit Count = 0
+  Other User Edit Percentage = 0
+  Revert Count = 0
+  Link Count = 0
+
+
+  # Pagename, User Count, Reliable User Count, Reliable User Percentage, Reliable User Edit Count, Reliable User Edit Percentage, Other User Count, Other User Percentage, Other User Edit Count, Other User Edit Percentage, Revert Count, Link Count
+
+  str = name.to_s+","+bot.to_s+","+edit_count.to_s+","+reverted_to.to_s+","+reverted_over.to_s+","+pages_count.to_s+","+rating.to_s+"\n"
+  File.open(user_file, "a"){|f| f.write(str)}
+
+puts "\n\nTime to complete this run: #{compute_time_taken(Time.now-start)}\n"
+print "\a\a\a\a\a\a\a\a\a\a"
