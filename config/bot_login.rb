980f64b1487523ead02fcceafc16395a85635a51
ChrisSalij <Chris@Salij.org>
2010-08-07 15:01:10 +0100

    Updated bot_login page to make clear what each field means and to include a link to where a bot can be registered

---
+++ config/bot_login.rb
@@ -0,0 +1,17 @@
+# This file contains the login information for your bot.
+
+# This information is not required, however if you have
+# registered a bot with Wikipedia, I highly recommend you 
+# use it. It will allow more results per query and will
+# speed up the runtime when fetching page information
+
+# See the following link for more information
+# http://en.wikipedia.org/wiki/Wikipedia:Bots
+
+@user_name  = "" # The username of your registered bot.
+@password   = "" # The password for your registered bot
+
+# This is the user agent string you provide when connecting to wikipedia.
+# There is no need to change this, but if you understand what a user agent
+# is, feel free to modify it.
+@user_agent = "#{@user_name} agent" 
